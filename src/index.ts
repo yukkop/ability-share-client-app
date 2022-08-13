f0122483a33a65eeeaeedc887a70b86e25ffb7d0
ISNUFFI <rozov2004@list.ru>
2022-08-13 21:56:46 +0300

    feat: added request to localhost:2999

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron')
+const { dialog } = require('electron')
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -9,6 +11,27 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
+app.commandLine.appendSwitch('ignore-certificate-errors')
 app.whenReady().then(() => {
+  console.log(123)
   createWindow()
+  const { net } = require('electron')
+
+  let data:any
+  const request = net.request({
+    method: "GET",
+    // protocol: "http:",
+    // hostname: "127.0.0.1:2999",
+    url: "https://127.0.0.1:2999/liveclientdata/activeplayer/",
+    
+  })
+  console.log(request)
+  request.on('response', (response:any) => {
+    console.log(response.statusCode)
+    console.log(JSON.stringify(response.headers))
+    response.on('data', (chunk:any) => {
+        data = chunk
+    })
+  })
+  request.end()
 })
