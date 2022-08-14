5e9a4414ccf1e703b081796f14a009a2f78d860d
ISNUFFI <rozov2004@list.ru>
2022-08-14 03:46:29 +0300

    feat: added overlay

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-const { app, BrowserWindow } = require('electron')
+import { isTypeParameterDeclaration } from "typescript"
+
+const { app, BrowserWindow, ipcMain } = require('electron')
 const { dialog } = require('electron')
+require("electron-reload")
 
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 300,
+    height: 200,
+    maxHeight: 200, minHeight: 200,
+    maxWidth: 300, minWidth: 300,
+    frame: false, 
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: __dirname + "\\preload.js"
+    },
+    transparent: true,
+    alwaysOnTop: true
   })
+  ipcMain.on("close-app", () => app.quit())
 
   win.loadFile('index.html')
 }
@@ -27,8 +40,8 @@ app.whenReady().then(() => {
   })
   console.log(request)
   request.on('response', (response:any) => {
-    console.log(response.statusCode)
-    console.log(JSON.stringify(response.headers))
+    // console.log(response.statusCode)
+    // console.log(JSON.stringify(response.headers))
     response.on('data', (chunk:any) => {
         data = chunk
     })
---
+++ src/renderer.js
@@ -0,0 +1,6 @@
+const CLOSE_BTN = document.getElementById("close")
+
+
+CLOSE_BTN?.addEventListener("click", () => {
+    api.close();
+})
